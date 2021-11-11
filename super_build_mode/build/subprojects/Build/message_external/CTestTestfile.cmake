f1cbd896e7d84e592f623439dc338bd0fa8cd107
NeverGiu <nevergiu@qq.com>
2021-11-11 18:42:03 +0800

    cmake-professional has been updated compiletly by nevergiu in 2021/11/11 at 18:41

--- super_build_mode/build/subprojects/Build/message_external/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nevergiu/Program/source/repos/nglive_box/cmake_professional/super_build_mode/build/subprojects/Source/message_external
-# Build directory: /home/nevergiu/Program/source/repos/nglive_box/cmake_professional/super_build_mode/build/subprojects/Build/message_external
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
---
+++ super_install_build/sup-release-build.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# filename debugBuild.sh
+
+# Cache required paths
+# --------------------
+readonly rootDir=`pwd`
+readonly buildDir="$rootDir/build"
+readonly srcDir="$rootDir/src"
+
+readonly logPath="$rootDir/log"
+readonly mainCmakePath="$rootDir/CMakeLists.txt"
+readonly srcCmakePath="$srcDir/CMakeLists.txt"
+
+if [ ! -f $logPath ]
+then
+	touch $logPath
+fi
+
+if [ ! -d $buildDir ]
+then
+	mkdir -p $buildDir
+fi
+
+if [ -d $buildDir ] && [ -f $srcCmakePath ] && [ -f $mainCmakePath ]
+then
+	cd $buildDir
+
+	# cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$HOME/TestApp/sup-install-build ..
+	cmake -G "Unix Makefiles" ..
+	# cmake --build . --target package_source # generating source package
+	echo "--(Install Message): You can input 'cmake --build . --target [modename]' to install the project by differents way below(make sure current directory is 'build')."
+	cmake --build . --target help # generating an targets list
+	# cmake --build . --target install
+	# ctest -v
+	# ninja test -v
+	# ctest --output-on-failure -L foo --repeat-until-fail 3
+	# ctest --output-on-failure
+	# Run others except 'barTest', Run just 'fooTest' #
+	# ctest -E 'barTest' -R 'fooTest'
+	
+else
+	if [ -f $logPath ]
+	then
+		currentTime=`date '+%F %T'`
+		echo "Error: Cmake build(debug version) is interrupted..Please check." >> $logPath
+		exit 0
+	fi
+fi
+
+exit 0
